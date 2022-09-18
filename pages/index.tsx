1e4aacd15d17403b9e46d75e5485a856d127d6b1
Azania Mokhampane <azaniam04@gmail.com>
2022-09-18 18:11:51 +0200

    feat: add a nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import { Spacer, Switch } from "@nextui-org/react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import NavBar from "../components/NavBar";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
