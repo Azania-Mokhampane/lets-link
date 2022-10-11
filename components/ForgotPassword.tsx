d8d14a626d2817e28c627a1044f8b6e99fa738fa
Azania Mokhampane <azaniam04@gmail.com>
2022-10-11 22:20:37 +0200

    feat: add form validations

--- components/ForgotPassword.tsx
+++ components/ForgotPassword.tsx
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
-import { Button, Card, Input, Row, Text } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Input,
+  Loading,
+  Row,
+  Spacer,
+  Text,
+} from "@nextui-org/react";
 import Modal from "./UI/Modal";
+import * as Yup from "yup";
 import { useResetPassword } from "@nhost/nextjs";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = Yup.object({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Please enter your email"),
+});
+
+interface IForgotPasswordFormProps {
+  email: string;
+}
 
 const ForgotPassword = () => {
   const [open, setOpen] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IForgotPasswordFormProps>({
+    defaultValues: {
+      email: "",
+    },
+    resolver: yupResolver(schema),
+  });
   const { resetPassword, isLoading, isSent, isError, error } =
     useResetPassword();
 
-  console.log({ isLoading, isSent, isError, error });
-
-  const handleFormSubmit = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-
-    await resetPassword(email, {
+  const handleFormSubmit = async (values: IForgotPasswordFormProps) => {
+    await resetPassword(values.email, {
       redirectTo: "http://localhost:3000/settings/change-password",
     });
   };
@@ -34,15 +61,18 @@ const ForgotPassword = () => {
         modalTitle="Reset Password"
       >
         {" "}
-        <form onSubmit={handleFormSubmit}>
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
           <Card.Body css={{ py: "$10" }}>
             <Input
               clearable
               bordered
               fullWidth
-              onChange={(e) => setEmail(e.target.value)}
               placeholder="Please enter you email"
+              {...register("email")}
             />
+            {errors.email ? (
+              <Text color="error">{errors.email.message}</Text>
+            ) : null}
           </Card.Body>
           <Card.Footer>
             <Row justify="space-evenly">
@@ -50,7 +80,15 @@ const ForgotPassword = () => {
                 Cancel
               </Button>
               <Button type={"submit"} auto shadow color={"secondary"} size="sm">
-                Reset Password
+                {isLoading ? (
+                  <Row justify="space-evenly">
+                    <Text color={"white"}>Resetting</Text>
+                    <Spacer />
+                    <Loading type="spinner" color={"white"} size="lg" />
+                  </Row>
+                ) : (
+                  "Send Request"
+                )}
               </Button>
             </Row>
           </Card.Footer>{" "}
